afdee01993b26fd6ebc68331c86fa95b9f1b987d
Mauricio Fragoso <mauricioj.fragoso@msn.com>
2023-08-14 21:20:24 -0300

    agenda

---
+++ dist/js/app.js
@@ -0,0 +1,26 @@
+import { Contato } from "./model/contato.js";
+let contatos = [];
+document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.getElementById('formulario-de-cadastro');
+    formulario.onsubmit = submeterFormulario;
+});
+function submeterFormulario(evento) {
+    evento.preventDefault();
+    const nome = document.getElementById('nome');
+    const fone = document.getElementById('fone');
+    const contato = new Contato(nome.value, fone.value);
+    contatos.push(contato);
+    atualizarListaDeContatos(contatos);
+    nome.value = '';
+    fone.value = '';
+    nome.focus();
+}
+function atualizarListaDeContatos(contatos) {
+    const listaDeContatos = document.querySelector('#lista-decontatos tbody');
+    listaDeContatos.innerHTML = '';
+    contatos.forEach(contato => {
+        const linhaDaTabela = listaDeContatos.insertRow();
+        linhaDaTabela.insertCell().textContent = contato.nome;
+        linhaDaTabela.insertCell().textContent = contato.fone;
+    });
+}
